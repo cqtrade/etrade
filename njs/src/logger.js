55a7140ee4d57548fcfba6760afd099ff7984871
Sander Mets <sandermets@live.com>
2023-04-07 11:12:55 +0300

    Set delay 1000 in logger

--- njs/src/logger.js
+++ njs/src/logger.js
@@ -1,7 +1,7 @@
 const createLogger = require('./createLogger.js')
 
 const logger = createLogger({
-    messageDelay: 5000,
+    messageDelay: 1000,
     discordLogLevels: ['info', 'warn', 'error'],
     discordWebhookUrl: process.env.DISCORD_WEBHOOK_URL,
 })
