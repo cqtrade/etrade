44f4f025f23d66934e477247fb61958198405205
Sander Mets <sandermets@live.com>
2023-04-09 16:08:10 +0300

    Fix floating error in qty size (#18)

--- njs/src/utils.js
+++ njs/src/utils.js
@@ -2,3 +2,21 @@ const sleep = (delay) => new Promise((resolve) =>
     setTimeout(resolve, delay))
 
 module.exports.sleep = sleep
+
+const countDecimals = (num) => {
+    const numAsString = num.toString();
+
+    // Check if the number has a decimal point
+    if (numAsString.includes('.')) {
+        return numAsString.split('.')[1].length
+    }
+
+    return 0
+}
+
+module.exports.countDecimals = countDecimals
+
+const fixedDecimals = (num, decimalPlaces) =>
+    parseFloat(num.toFixed(decimalPlaces))
+
+module.exports.fixedDecimals = fixedDecimals
