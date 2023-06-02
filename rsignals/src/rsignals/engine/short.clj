84a206bd1784db1051128e985bd484d883ce53bc
Sander Mets <sandermets@live.com>
2023-06-02 22:07:43 +0300

    Add more tickers (#29)

--- rsignals/src/rsignals/engine/short.clj
+++ rsignals/src/rsignals/engine/short.clj
@@ -1,7 +1,8 @@
 (ns rsignals.engine.short
   (:require
    [rsignals.engine.ohlc :as ohlc]
-   [rsignals.engine.ta :as ta]))
+   [rsignals.engine.ta :as ta]
+   [clojure.pprint :as pprint]))
 
 (defn prep-datasets
   [xs]
@@ -16,7 +17,7 @@
          vals
          (mapv #(vec (sort-by :time %))))))
 
-(def skip-bars 75)
+(def skip-bars 50)
 
 (defn indicators
   [{:keys [tdfi-p rex-p rex-sp conf-p tpcoef slcoef risk]} coll]
@@ -181,7 +182,9 @@
 
 (defn signals
   [t-args xss]
-  (let [xss-prepped (prep-datasets xss)]
+  (let [xss-prepped (->> xss
+                         (filter #(> (count %) 70))
+                         prep-datasets)]
     (doall (pmap #(e-indies % xss-prepped) [t-args]))))
 
 (defn get-quotas
@@ -223,9 +226,30 @@
                  "LTCUSDT"
                  "MATICUSDT"
                  "DOTUSDT"
-                 "ATOMUSDT"]
+                 "ATOMUSDT"
+                 "LINAUSDT"
+                 "APTUSDT"
+                 "ARBUSDT"
+                 "INJUSDT"
+                 "RNDRUSDT"]
         xss (get-quotas interval tickers)]
     (->> xss
          (signals t-args)
          (mapv (fn [x] (mapv last x)))
-         flatten)))
+         flatten
+         (remove #(nil? (:sig %))))))
+
+(comment
+
+  (let [t-args {:tdfi-p 1
+                :tdfi-level 1
+                :rex-p 1
+                :rex-sp 1
+                :conf-p 1
+                :conf-cross 1
+                :tpcoef 1
+                :slcoef 1
+                :risk 1}]
+    (clojure.pprint/pprint (get-signals t-args)))
+
+  1)
