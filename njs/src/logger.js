d6dfaf71805f0bb4316804fb0e43e14add4aa7a0
Kaspar Vihm <kaspar.vihm@gmail.com>
2023-07-25 23:04:11 +0300

    Support additional webhook url for logging to discord

--- njs/src/logger.js
+++ njs/src/logger.js
@@ -2,8 +2,10 @@ const createLogger = require('./createLogger.js')
 
 const logger = createLogger({
     messageDelay: 1000,
-    discordLogLevels: ['info', 'warn', 'error'],
+    discordLogLevels: ['info', 'warn'],
     discordWebhookUrl: process.env.DISCORD_WEBHOOK_URL,
+    additionalDiscordLogLevels: ['error'],
+    additionalDiscordWebhookUrl: process.env.DISCORD_ADDITIONAL_WEBHOOK_URL,
 })
 
 module.exports = logger
