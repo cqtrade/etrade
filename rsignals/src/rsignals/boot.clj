7ce30be345284d5ea6224c2061badb468444d8f5
Sander Mets <sandermets@live.com>
2024-02-25 23:51:58 +0200

    Log params on boot (#65)

--- rsignals/src/rsignals/boot.clj
+++ rsignals/src/rsignals/boot.clj
@@ -53,9 +53,9 @@
   []
   (prn "############## START BOOT ##############")
   (d-envs/print-envs)
+  (d-envs/get-dynamic-tickers-vol)
   (d-envs/log-boot-long)
   (d-envs/log-boot-short)
-  (d-envs/get-dynamic-tickers-vol)
   (h4-envs/print-envs)
   (async/go (discord/loop-messages))
   (worker))
