49906b8764ef5770930b480e364e9685d1083d8b
Kaspar Vihm <kaspar.vihm@gmail.com>
2024-04-12 23:34:57 +0300

    Update and use values from env

--- njs/src/index.js
+++ njs/src/index.js
@@ -1,8 +1,17 @@
-const positionCheck = require('./tradebb/position-check.js')
-const server = require('./server.js')
+const positionCheck = require("./tradebb/position-check.js");
+const positionCheckBN = require("./tradebn/position-check.js");
+
+const server = require("./server.js");
 
 // USDT positions check
-positionCheck.engine()
+console.log(process.env);
+if (process.env.API_ENABLED) {
+  positionCheck.engine();
+}
+
+if (process.env.BN_API_ENABLED) {
+  positionCheckBN.engine();
+}
 
 // start server - incoming signals
-server.bootServer()
+server.bootServer();
--- njs/src/server.js
+++ njs/src/server.js
@@ -1,66 +1,63 @@
-const fastify = require('fastify');
-const trade = require('./tradebb/index')
-const logger = require('./logger.js')
-const { sleep } = require('./utils.js')
+const fastify = require("fastify");
+const trade = require("./tradebb/index");
+// const tradeBn = require("./tradebn/index");
+const logger = require("./logger.js");
+const { sleep } = require("./utils.js");
 
 const server = fastify();
 
-server.get('/health', async (request, reply) => 'OK');
-server.get('/', async (request, reply) => '');
+server.get("/health", async (request, reply) => "OK");
+server.get("/", async (request, reply) => "");
 
-server.post(
-    '/signal',
-    {},
-    async (request, reply) => {
-        reply.code(201).send({ status: 'OK' });
-        setTimeout(() => {
-            (async () => {
-                try {
-                    console.log('request.body', request.body);
-                    if (request.body instanceof Array) {
-                        for (const sig of request.body) {
-                            await trade.signalHandler(sig);
-                            await sleep(1000);
-                        }
-                        logger.info(`Signals received: ${request.body.length}`);
-                        return;
-                    }
-                } catch (e) {
-                    logger.error('signal handler error ' + e.message);
-                }
-            })().catch();
-        }, 0);
-    });
+server.post("/signal", {}, async (request, reply) => {
+  reply.code(201).send({ status: "OK" });
+  setTimeout(() => {
+    (async () => {
+      try {
+        console.log("request.body", request.body);
+        if (request.body instanceof Array) {
+          for (const sig of request.body) {
+            // TODO: Use correct signal handler
+            // await tradeBn.signalHandler(sig)
+            await trade.signalHandler(sig);
+            await sleep(1000);
+          }
+          logger.info(`Signals received: ${request.body.length}`);
+          return;
+        }
+      } catch (e) {
+        logger.error("signal handler error " + e.message);
+      }
+    })().catch();
+  }, 0);
+});
 
-    server.post(
-        '/signal4',
-        {},
-        async (request, reply) => {
-            reply.code(201).send({ status: 'OK' });
-            setTimeout(() => {
-                (async () => {
-                    try {
-                        console.log('request.body', request.body);
-                        if (request.body instanceof Array) {
-                            // TODO: handle signal
-                            logger.info(`4 Signals received: ${request.body.length}`);
-                            return;
-                        }
-                    } catch (e) {
-                        logger.error('4 signal handler error ' + e.message);
-                    }
-                })().catch();
-            }, 0);
-        });
+server.post("/signal4", {}, async (request, reply) => {
+  reply.code(201).send({ status: "OK" });
+  setTimeout(() => {
+    (async () => {
+      try {
+        console.log("request.body", request.body);
+        if (request.body instanceof Array) {
+          // TODO: handle signal
+          logger.info(`4 Signals received: ${request.body.length}`);
+          return;
+        }
+      } catch (e) {
+        logger.error("4 signal handler error " + e.message);
+      }
+    })().catch();
+  }, 0);
+});
 
 function bootServer() {
-    server.listen(3000, '0.0.0.0', (err, address) => {
-        if (err) {
-            console.error(err.message);
-            process.exit(1);
-        }
-        console.log(`Server listening at ${address}`);
-    });
+  server.listen(3000, "0.0.0.0", (err, address) => {
+    if (err) {
+      console.error(err.message);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  });
 }
 
 module.exports.bootServer = bootServer;
