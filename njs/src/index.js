dc305426f6ed82e36dc7f32da8d4d4984f9556f0
Kaspar Vihm <kaspar.vihm@gmail.com>
2024-04-12 23:39:39 +0300

    Remove console.log

--- njs/src/index.js
+++ njs/src/index.js
@@ -4,7 +4,6 @@ const positionCheckBN = require("./tradebn/position-check.js");
 const server = require("./server.js");
 
 // USDT positions check
-console.log(process.env);
 if (process.env.API_ENABLED) {
   positionCheck.engine();
 }
