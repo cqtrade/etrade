024d89c11ff7b107614d42504a43cd410c26e2a6
seppkristen@gmail.com <seppkristen@gmail.com>
2024-05-26 17:06:12 +0300

    Binance signaalid, positsioonile sisenemine/väljumine Node s

---
+++ njs/src/tradebn/reqs.js
@@ -0,0 +1,137 @@
+const { USDMClient } = require('binance');
+const positionCheck = require('./position-check.js');
+
+const errorFilter = (error) => {
+	return error.message || error.body.msg
+		? error.message ?? error.body.msg
+		: error;
+};
+
+const client = new USDMClient({
+	api_key: process.env.BN_API_KEY,
+	api_secret: process.env.BN_API_SECRET,
+	strict_param_validation: true,
+});
+
+const getSymbolTicker = async (symbol) => {
+	try {
+		const result = await client.get24hrChangeStatistics({ symbol });
+
+		return result;
+	} catch (error) {
+		console.error('getSymbolTicker failed: ', errorFilter(error));
+		throw error;
+	}
+};
+
+module.exports.getSymbolTicker = getSymbolTicker;
+
+const getInstrumentInfo = async (symbol) => {
+	try {
+		const result = await positionCheck.getExchangeInfoForSymbol(symbol);
+
+		return result;
+	} catch (error) {
+		console.error(
+			'getApiQuantitativeRulesIndicators failed: ',
+			errorFilter(error),
+		);
+		throw error;
+	}
+};
+
+module.exports.getInstrumentInfo = getInstrumentInfo;
+
+const setPricePrecisionByTickSize = (price, tickSize) => {
+	const precision = tickSize.toString().split('.')[1].length - 1;
+	return Number(price).toFixed(precision);
+};
+
+module.exports.setPricePrecisionByTickSize = setPricePrecisionByTickSize;
+
+const submitOrder = async ({
+	side,
+	symbol,
+	quantity,
+	type,
+	timeInForce,
+	stopPrice,
+	workingType = 'MARK_PRICE',
+	price,
+	reduceOnly,
+	positionSide,
+	closePosition,
+	cancelOrders,
+}) => {
+	if (cancelOrders) {
+		try {
+			const result = await client.cancelAllOpenOrders({ symbol });
+			console.debug(result.msg);
+		} catch (error) {
+			console.error('cancelOpenOrders failed: ', errorFilter(error));
+			throw error;
+		}
+	}
+
+	try {
+		let orderParams = { symbol, side, type };
+
+		if (quantity) {
+			orderParams = { ...orderParams, quantity };
+		}
+		if (timeInForce) {
+			orderParams = { ...orderParams, timeInForce };
+		}
+		if (stopPrice) {
+			orderParams = { ...orderParams, stopPrice };
+		}
+		if (workingType) {
+			orderParams = { ...orderParams, workingType };
+		}
+		if (price) {
+			orderParams = { ...orderParams, price };
+		}
+		if (reduceOnly) {
+			orderParams = { ...orderParams, reduceOnly };
+		}
+		if (positionSide) {
+			orderParams = { ...orderParams, positionSide };
+		}
+		if (closePosition) {
+			orderParams = { ...orderParams, closePosition };
+		}
+
+		const result = await client.submitNewOrder(orderParams);
+
+		return result;
+	} catch (error) {
+		console.error('submitOrder failed: ', errorFilter(error));
+		throw error;
+	}
+};
+
+module.exports.submitOrder = submitOrder;
+
+// TODO implement this to tradeBN
+const setLeverage = async ({ symbol, buyLeverage, sellLeverage }) => {
+	try {
+		const { retMsg, result } = await client.setLeverage({
+			category: 'linear',
+			symbol,
+			buyLeverage,
+			sellLeverage,
+		});
+		if (retMsg !== 'OK') {
+			// TODO it shouldn't matter at this point if fails
+			// throw new Error('setLeverage failed ' + retMsg);
+			console.log('setLeverage failed: ', retMsg);
+		}
+
+		return result;
+	} catch (error) {
+		// TODO should be logged
+		console.log('setLeverage failed: ', error);
+	}
+};
+
+module.exports.setLeverage = setLeverage;
