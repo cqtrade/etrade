3ad737af88a48ecbef0d8c001705fcb5d164395d
Sander Mets <sandermets@live.com>
2024-06-11 22:40:52 +0300

    Fix clj boot

--- rsignals/src/rsignals/boot.clj
+++ rsignals/src/rsignals/boot.clj
@@ -1,56 +1,56 @@
 (ns rsignals.boot
-   (:require [clojure.core.async :as async]
+  (:require [clojure.core.async :as async]
             ; rsignals.engine.core Bybit
-             [rsignals.engine.core :as engine-bybit]
-             [rsignals.engine.envs :as bybit-envs]
+            [rsignals.engine.core :as engine-bybit]
+            [rsignals.engine.envs :as bybit-envs]
             ; rsignals.engine4.core Binance 
-             [rsignals.engine4.core :as engine-binance]
-             [rsignals.engine4.envs :as binance-envs]
-             [rsignals.tools.discord :as discord]
-             [rsignals.utils :as utils]))
+            [rsignals.engine4.core :as engine-binance]
+            [rsignals.engine4.envs :as binance-envs]
+            [rsignals.tools.discord :as discord]
+            [rsignals.utils :as utils]))
 
 
- #_(def test-time
-     (System/getenv "TEST_TIME"))
+#_(def test-time
+    (System/getenv "TEST_TIME"))
 
- (def the-daily-times
-   #{"235510"})
+(def the-daily-times
+  #{"235510"})
 
- #_(def the-4hourly-times
-     #{test-time
-       "35613"
-       "75613"
-       "115613"
-       "155613"
-       "195613"})
+#_(def the-4hourly-times
+    #{test-time
+      "35613"
+      "75613"
+      "115613"
+      "155613"
+      "195613"})
 
- (defn worker
-   []
-   (let [t (utils/getTimeInUTC)
-         c (str (:h t) (:m t) (:s t))
-         daily-time? (the-daily-times c)
-         #_#_hourly4-time? (the-4hourly-times c)]
+(defn worker
+  []
+  (let [t (utils/getTimeInUTC)
+        c (str (:h t) (:m t) (:s t))
+        daily-time? (the-daily-times c)
+        #_#_hourly4-time? (the-4hourly-times c)]
 
-     (when (System/getenv "API_ENABLED")
-       (when daily-time?
-         (prn "BYBIT SIGNALS START" t)
-         (engine-bybit/engine)
-         (prn "BYBIT SIGNALS DONE")
-         (Thread/sleep 103))))
+    (when (System/getenv "API_ENABLED")
+      (when daily-time?
+        (prn "BYBIT SIGNALS START" t)
+        (engine-bybit/engine)
+        (prn "BYBIT SIGNALS DONE")
+        (Thread/sleep 103)))
 
-   (when (System/getenv "BN_API_ENABLED")
-     (when daily-time?
-       (prn "BINANCE SIGNALS START" t)
-       (engine-binance/engine)
-       (prn "BINANCE SIGNALS DONE")
-       (Thread/sleep 103))
+    (when (System/getenv "BN_API_ENABLED")
+      (when daily-time?
+        (prn "BINANCE SIGNALS START" t)
+        (engine-binance/engine)
+        (prn "BINANCE SIGNALS DONE")
+        (Thread/sleep 103))
 
-     #_(when hourly4-time?
-         (prn "BINANCE SIGNALS" t)
-         (engine-binance/engine)))
+      #_(when hourly4-time?
+          (prn "BINANCE SIGNALS" t)
+          (engine-binance/engine)))
 
-   (Thread/sleep 1000)
-   (recur)))
+    (Thread/sleep 1000)
+    (recur)))
 
 (defn start
   []
