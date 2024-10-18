a9ab4f52026e0ed23eae546e9ef0ffce7544270c
Kaspar Vihm <kaspar.vihm@gmail.com>
2024-10-18 16:38:55 +0300

    Implement Kraken Futures API client

---
+++ njs/src/tradekn/api.js
@@ -0,0 +1,282 @@
+const axios = require('axios');
+const crypto = require('crypto');
+const utf8 = require('utf8');
+const qs = require('qs');
+
+const KrakenFuturesApiClient = ({
+	apiKey,
+	apiSecret,
+	isTestnet = false,
+	timeout,
+}) => {
+	if (!apiKey || !apiSecret) {
+		throw new Error('API Key and API Secret are required.');
+	}
+
+	const BASE_URL = isTestnet
+		? 'https://demo-futures.kraken.com'
+		: 'https://futures.kraken.com';
+
+	let nonceCounter = 0;
+
+	const createNonce = () => {
+		const timestamp = Date.now();
+
+		nonceCounter = (nonceCounter + 1) % 10000;
+
+		return `${timestamp}${String(nonceCounter).padStart(5, '0')}`;
+	};
+
+	const isEmpty = (object) =>
+		object == null || Object.keys(object).length === 0;
+
+	// Reference: https://docs.kraken.com/api/docs/guides/futures-rest#authent
+	const signRequest = (endpoint, nonce, postData = '') => {
+		// Step 1: Extract the path from the endpoint starting from '/api' and
+		// concatenate postData + Nonce + sanitized endpointPath
+		const endpointPath = endpoint.slice(endpoint.indexOf('/api'));
+		const concatenatedMessage = `${postData}${nonce}${endpointPath}`;
+
+		// Step 2: Hash the result of step 1 with the SHA-256 algorithm
+		const sha256Hash = crypto
+			.createHash('sha256')
+			.update(utf8.encode(concatenatedMessage))
+			.digest();
+
+		// Step 3: Base64-decode your apiSecret
+		const decodedApiSecret = Buffer.from(apiSecret, 'base64');
+
+		// Step 4: Use the result of step 3 to hash the result of step 2 with the HMAC-SHA-512 algorithm
+		const hmacSha512Hash = crypto
+			.createHmac('sha512', decodedApiSecret)
+			.update(sha256Hash)
+			.digest();
+
+		// Step 5: Base64-encode the result of step 4
+		return hmacSha512Hash.toString('base64');
+	};
+
+	const createHeaders = ({ isPrivate, method, endpoint, data }) => {
+		const nonce = isPrivate ? createNonce() : null;
+		const baseHeaders = { Accept: 'application/json' };
+		const hasData = !isEmpty(data);
+
+		const privateHeaders = isPrivate
+			? {
+					APIKey: apiKey,
+					Nonce: nonce,
+					Authent: signRequest(
+						endpoint,
+						nonce,
+						hasData ? qs.stringify(data) : '',
+					),
+			  }
+			: {};
+
+		const contentTypeHeader = hasData
+			? { 'Content-Type': 'application/x-www-form-urlencoded' }
+			: {};
+
+		return { ...baseHeaders, ...privateHeaders, ...contentTypeHeader };
+	};
+
+	const createRequestConfig = ({ method, endpoint, headers, data }) => ({
+		method,
+		maxBodyLength: Infinity,
+		url: `${BASE_URL}${endpoint}`,
+		headers,
+		timeout,
+		...(!isEmpty(data) && { data: qs.stringify(data) }),
+	});
+
+	const handleRequestError = (error, method, endpoint) => {
+		const errorMessage =
+			error.response?.data || error.message || 'Unknown error occurred';
+
+		throw new Error(
+			`Error during ${method} request to ${endpoint}: ${errorMessage}`,
+		);
+	};
+
+	const makeRequest = async ({
+		method = 'GET',
+		endpoint,
+		data,
+		isPrivate = false,
+	}) => {
+		const headers = createHeaders({
+			isPrivate,
+			method,
+			endpoint,
+			data,
+		});
+
+		const config = createRequestConfig({
+			method,
+			endpoint,
+			headers,
+			data,
+		});
+
+		try {
+			const { data: responseData } = await axios.request(config);
+
+			return responseData;
+		} catch (error) {
+			handleRequestError(error, method, endpoint);
+		}
+	};
+
+	const makePrivateRequest = ({ method = 'GET', endpoint, data }) =>
+		makeRequest({ method, endpoint, data, isPrivate: true });
+
+	// Public endpoints
+	const getTickers = () =>
+		makeRequest({ endpoint: '/derivatives/api/v3/tickers' });
+
+	const getTickerBySymbol = (symbol) =>
+		makeRequest({ endpoint: `/derivatives/api/v3/tickers/${symbol}` });
+
+	const getInstruments = () =>
+		makeRequest({ endpoint: '/derivatives/api/v3/instruments' });
+
+	// Private endpoints
+	const getAccounts = () =>
+		makePrivateRequest({
+			endpoint: '/derivatives/api/v3/accounts',
+		});
+
+	const getOpenPositions = () =>
+		makePrivateRequest({
+			endpoint: '/derivatives/api/v3/openpositions',
+		});
+
+	const getOpenOrders = () =>
+		makePrivateRequest({
+			endpoint: '/derivatives/api/v3/openorders',
+		});
+
+	const sendOrder = ({
+		processBefore,
+		orderType,
+		symbol,
+		side,
+		size,
+		limitPrice,
+		stopPrice,
+		cliOrdId,
+		triggerSignal,
+		reduceOnly,
+		trailingStopMaxDeviation,
+		trailingStopDeviationUnit,
+		limitPriceOffsetValue,
+		limitPriceOffsetUnit,
+	}) =>
+		makePrivateRequest({
+			method: 'POST',
+			endpoint: '/derivatives/api/v3/sendorder',
+			data: {
+				processBefore,
+				orderType,
+				symbol,
+				side,
+				size,
+				limitPrice,
+				stopPrice,
+				cliOrdId,
+				triggerSignal,
+				reduceOnly,
+				trailingStopMaxDeviation,
+				trailingStopDeviationUnit,
+				limitPriceOffsetValue,
+				limitPriceOffsetUnit,
+			},
+		});
+
+	const batchOrder = ({ processBefore, batchOrderJson }) =>
+		makePrivateRequest({
+			method: 'POST',
+			endpoint: '/derivatives/api/v3/batchorder',
+			data: {
+				processBefore,
+				json: batchOrderJson,
+			},
+		});
+
+	const editOrder = ({
+		processBefore,
+		orderId,
+		cliOrdId,
+		size,
+		limitPrice,
+		stopPrice,
+		trailingStopMaxDeviation,
+		trailingStopDeviationUnit,
+	}) =>
+		makePrivateRequest({
+			method: 'POST',
+			endpoint: '/derivatives/api/v3/editorder',
+			data: {
+				processBefore,
+				orderId,
+				cliOrdId,
+				size,
+				limitPrice,
+				stopPrice,
+				trailingStopMaxDeviation,
+				trailingStopDeviationUnit,
+			},
+		});
+
+	const cancelOrder = (orderId, cliOrdId) =>
+		makePrivateRequest({
+			method: 'POST',
+			endpoint: '/derivatives/api/v3/cancelorder',
+			data: { order_id: orderId, cliOrdId },
+		});
+
+	const cancelAllOrders = (symbol) =>
+		makePrivateRequest({
+			method: 'POST',
+			endpoint: '/derivatives/api/v3/cancelallorders',
+			data: { symbol },
+		});
+
+	const cancelAllOrdersAfter = (timeoutInSeconds) =>
+		makePrivateRequest({
+			method: 'POST',
+			endpoint: '/derivatives/api/v3/cancelallordersafter',
+			data: { timeout: timeoutInSeconds },
+		});
+
+	const getLeveragePreferences = () =>
+		makePrivateRequest({
+			endpoint: '/derivatives/api/v3/leveragepreferences',
+		});
+
+	const setLeveragePreferences = ({ symbol, maxLeverage }) =>
+		makePrivateRequest({
+			method: 'PUT',
+			endpoint: `/derivatives/api/v3/leveragepreferences`,
+			data: { symbol, maxLeverage },
+		});
+
+	return {
+		getTickers,
+		getTickerBySymbol,
+		getInstruments,
+		getAccounts,
+		getOpenPositions,
+		getOpenOrders,
+		sendOrder,
+		batchOrder,
+		editOrder,
+		cancelOrder,
+		cancelAllOrders,
+		cancelAllOrdersAfter,
+		getLeveragePreferences,
+		setLeveragePreferences,
+	};
+};
+
+module.exports = KrakenFuturesApiClient;
