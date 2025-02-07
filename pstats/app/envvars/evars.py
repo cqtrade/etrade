b9a3df74258fd2e4618a0cd3d63e181601a8e73f
Sander Mets <sandermets@live.com>
2025-02-07 20:12:49 +0200

    Add foolsgold bot

--- pstats/app/envvars/evars.py
+++ pstats/app/envvars/evars.py
@@ -6,5 +6,8 @@ print(os.environ.get('P_DISCORD_STATS'))
 envdict = {
   "stats_enabled": bool(os.environ.get('P_STATS_ENABLED')),
   "discord_channel": os.environ.get('P_DISCORD_STATS'),
+  "foolsgold": bool(os.environ.get('FOOLSGOLD')),
+  "foolsgold_key": os.environ.get('FOOLSGOLD_KEY'),
+  "foolsgold_secret": os.environ.get('FOOLSGOLD_SECRET'),
 }
 
